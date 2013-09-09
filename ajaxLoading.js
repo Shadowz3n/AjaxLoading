37b8de53be684c1cccde59f72213c529da3379ee
henrique <henriquebs@vallery.com.br>
2013-09-09 10:57:02 -0300

    Primeiro commit

---
+++ ajaxLoading.js
@@ -0,0 +1,34 @@
+window.jQuery || document.write(unescape('%3Cscript src="http://jquery.com/jquery-wp-content/themes/jquery/js/jquery-1.9.1.min.js"%3E%3C/script%3E'))
+(function($, window, undefined){
+	var oldXHR = $.ajaxSettings.xhr;
+	$.ajaxSettings.xhr = function()
+	{
+		var xhr = oldXHR();
+		if(xhr instanceof window.XMLHttpRequest)
+		{
+			xhr.addEventListener('porcentagem', this.progress, false);
+		}
+		
+		if(xhr.upload)
+		{
+			xhr.upload.addEventListener('porcentagem', this.progress, false);
+		}
+		
+		return xhr;
+	};
+})(jQuery, window);
+
+var oldXHR = $.ajaxSettings.xhr;
+
+/* Modo de utilizar
+$.ajaxSetup({
+	porcentagem: function(e)
+	{
+		if(e.lengthComputable)
+		{
+			var pct = (e.loaded / e.total);
+			$(".loading").css({width:pct});
+		}
+	}
+});
+*/
