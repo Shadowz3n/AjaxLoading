70c5b2ec16aaa010e5880b33191ed9f5cb05db77
Henrique Bissoli <emp_shad@hotmail.com>
2014-11-14 10:44:34 -0200

    Update ajaxLoading.js

--- ajaxLoading.js
+++ ajaxLoading.js
@@ -1,23 +1,13 @@
 window.jQuery || document.write(unescape('%3Cscript src="http://jquery.com/jquery-wp-content/themes/jquery/js/jquery-1.9.1.min.js"%3E%3C/script%3E'))
 (function($, window, undefined){
 	var oldXHR = $.ajaxSettings.xhr;
-	$.ajaxSettings.xhr = function()
-	{
+	$.ajaxSettings.xhr = function(){
 		var xhr = oldXHR();
-		if(xhr instanceof window.XMLHttpRequest)
-		{
-			xhr.addEventListener('porcentagem', this.progress, false);
-		}
-		
-		if(xhr.upload)
-		{
-			xhr.upload.addEventListener('porcentagem', this.progress, false);
-		}
-		
+		return (xhr instanceof window.XMLHttpRequest)? xhr.addEventListener('porcentagem', this.progress, false) : true;
+		return (xhr.upload)? xhr.upload.addEventListener('porcentagem', this.progress, false) : true;
 		return xhr;
 	};
 })(jQuery, window);
-
 var oldXHR = $.ajaxSettings.xhr;
 
 /* Modo de utilizar
